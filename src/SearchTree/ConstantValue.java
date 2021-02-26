91289d65aee2a0a781df8553e5ff3270bd82e9fc
charin <charinwises@gmail.com>
2021-02-27 02:26:24 +0700

    delete var ROOT

--- src/SearchTree/ConstantValue.java
+++ src/SearchTree/ConstantValue.java
@@ -1,8 +1,6 @@
 package SearchTree;
 
 public class ConstantValue {
-    public static Node ROOT;
-
     public static int COUNT = 5;
      public static final int[] VALUES = {50, 19, 30, 20, 40, 70, 60, 80, 100, 101};
 }
